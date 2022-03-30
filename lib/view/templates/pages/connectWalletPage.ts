d99d5d2549621da409ea94f1ce7b22b1d37692c6
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-03-30 22:15:19 +0100

    new connect wallet page

--- lib/view/templates/pages/connectWalletPage.ts
+++ lib/view/templates/pages/connectWalletPage.ts
@@ -3,14 +3,18 @@ import { ConnectWalletLogo, MetamaskLogo } from "../components/logos";
 
 export const ConnectWalletPage = () =>
   html`
-    <div class="center height200">
-      <button
-        id="connectWalletButton"
-        class="labelButton width-200 marginTop-50 unselectable light-shadow"
-      >
-        <h4>Connect your wallet</h4>
-        <hr />
-        ${MetamaskLogo()}
-      </button>
+    <div class="intro-logo">
+      <div class="">Ricardian Fabric DAO</div>
+
+      <div class="center height200">
+        <button
+          id="connectWalletButton"
+          class="labelButton width-200 marginTop-50 unselectable light-shadow"
+        >
+          <h2>Connect Your Wallet!</h2>
+          ${ConnectWalletLogo()}
+          <hr />
+        </button>
+      </div>
     </div>
   `;
