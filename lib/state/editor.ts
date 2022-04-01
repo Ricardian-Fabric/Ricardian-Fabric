f45bd7d37c4525083684a4004e4b104a3a35e078
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-04-01 23:34:21 +0200

    added a new editor

--- lib/state/editor.ts
+++ lib/state/editor.ts
@@ -46,3 +46,50 @@ export default function createNewEditor(): MediumEditor {
     },
   });
 }
+
+export function createProposalEditor(): MediumEditor {
+  //@ts-ignore
+  return new MediumEditor(".proposal-editor", {
+    imageDragging: false,
+    toolbar: {
+      /* These are the default options for the toolbar,
+		 if nothing is passed this is what is used */
+      allowMultiParagraphSelection: true,
+      buttons: [
+        "bold",
+        "italic",
+        "underline",
+        "anchor",
+        "anchor",
+        "quote",
+        "pre",
+        "orderedlist",
+        "unorderedlist",
+        "indent",
+        "outdent",
+        "justifyLeft",
+        "justifyCenter",
+        "justifyRight",
+        "justifyFull",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+      ],
+      firstButtonClass: "medium-editor-button-first",
+      lastButtonClass: "medium-editor-button-last",
+      standardizeSelectionStart: true,
+      static: false,
+      /* options which only apply when static is true */
+      align: "center",
+      sticky: false,
+      updateOnEmptySelection: false,
+    },
+    placeholder: {
+      text: "Description...",
+      hideOnClick: false,
+    },
+  });
+}
