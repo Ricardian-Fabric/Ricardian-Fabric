fc38a5ec15935076e72c5b15b35f8eaacc7994be
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-05-20 22:56:29 +0100

    tokens sold is 2 million

--- lib/business/actions/dashboardActions.ts
+++ lib/business/actions/dashboardActions.ts
@@ -122,9 +122,8 @@ export async function dashboardActions(props: State) {
 
   const tokensSold = tokensSoldOptions.data;
   // tokensSold = ricLeft; // uncomment to see the sale finished UI, redeclare tokensSold with let;
-  // TODO: CHANGE THE TOKENS SOLD , NOW I GOT CHAIN SPECIFIC OneRIC!!
 
-  if (tokensSold === "40000000") {
+  if (tokensSold === "2000000") {
     dispatch_renderLoadedValue(props, "Sale finished", ricLeftEl);
   } else {
     dispatch_renderLoadedValue(props, ricLeft, ricLeftEl);
@@ -138,11 +137,10 @@ export async function dashboardActions(props: State) {
   }
 
   const ricRate = ricRateOptions.data;
-  // TODO: CHANGE THE TOKENS SOLD , NOW I GOT CHAIN SPECIFIC OneRIC!!
-  if (tokensSold === "40000000") {
+  if (tokensSold === "2000000") {
     dispatch_renderLoadedValue(props, "Sale finished", ricSaleRateEl);
   } else {
-    dispatch_renderLoadedValue(props, ricRate + " OneRIC/ONE", ricSaleRateEl);
+    dispatch_renderLoadedValue(props, ricRate, ricSaleRateEl);
   }
 
   const vaultOptions = await OptionsBuilder(() => getRicVaultContract());
