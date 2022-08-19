79030599a6bdb13252444682e9a957e3498c081c
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-08-19 13:12:30 +0100

    refactored menu buttons

--- lib/view/templates/pages/menuPage.ts
+++ lib/view/templates/pages/menuPage.ts
@@ -12,95 +12,40 @@ import {
 } from "../components/logos";
 
 export const MenuPage = (state: State) => {
-  const DeploymentsPage = () => html` <hr />
-    <div class="text-align-center">
-      <button
-        class="lightSlateGray-shadow  labelButton"
-        id="dashboard-button"
-        title="Dashboard"
-      >
-        ${Dashboard2()}
-      </button>
-    </div>
-    <div class="text-align-center">
-      <button
-        class="lightSlateGray-shadow  labelButton"
-        id="create-contract-button"
-        title="Create a Ricardian contract"
-      >
-        ${AgreementLogo()}
-      </button>
-    </div>
+  const DeploymentsPage = () => html`<hr />
 
-    <div class="text-align-center">
-      <button
-        class="lightSlateGray-shadow  labelButton"
-        id="smart-contract-catalog-button"
-        title="Catalogue"
-      >
-        ${catalogLogo()}
-      </button>
-    </div>
-    <div class="text-align-center">
-      <button
-        title="Trails"
-        class="lightSlateGray-shadow labelButton"
-        id="trails-page-button"
-      >
-        ${TrailsLogo()}
-      </button>
-    </div>`;
+<div class="text-align-center">
+  <button class="lightSlateGray-shadow  labelButton" id="create-contract-button" title="Create a Ricardian contract">
+    ${AgreementLogo()}
+  </button>
+</div>
+<div class="text-align-center">
+  <button title="Trails" class="lightSlateGray-shadow labelButton" id="trails-page-button">
+    ${TrailsLogo()}
+  </button>
+</div>`;
   const DAOPage = () => html`
     <div class="text-align-center">
-      <button
-        class="lightSlateGray-shadow  labelButton"
-        id="dashboard-button"
-        title="Dashboard"
-      >
+      <button class="lightSlateGray-shadow  labelButton" id="dashboard-button" title="Dashboard">
         ${Dashboard2()}
       </button>
     </div>
     <div class="text-align-center">
-      <button
-        title="DAO"
-        class="lightSlateGray-shadow  labelButton"
-        id="review-and-vote-button"
-      >
+      <button title="DAO" class="lightSlateGray-shadow  labelButton" id="review-and-vote-button">
         ${VotingLogo()}
       </button>
-      <!-- <div class="text-align-enter">
-        <button
-          title="Token Sale"
-          class="lightSlateGray-shadow labelButton"
-          id="tokensale-button"
-        >
-          ${TokenSaleLogo()}
-        </button>
-      </div> -->
       <div class="text-align-center">
-        <button
-          title="Fees"
-          class="lightSlateGray-shadow labelButton"
-          id="fees-button"
-        >
+        <button title="Fees" class="lightSlateGray-shadow labelButton" id="fees-button">
           ${RewardsPageIcon()}
         </button>
       </div>
       <div class="text-align-center">
-        <button
-          title="Vault"
-          class="lightSlateGray-shadow labelButton"
-          id="vault-button"
-        >
+        <button title="Vault" class="lightSlateGray-shadow labelButton" id="vault-button">
           ${VaultLogo()}
         </button>
       </div>
       <div class="text-align-center">
-        <button
-          title="Trails"
-          class="lightSlateGray-shadow labelButton"
-          id="trails-page-button"
-        >
+        <button title="Trails" class="lightSlateGray-shadow labelButton" id="trails-page-button">
           ${TrailsLogo()}
         </button>
       </div>
