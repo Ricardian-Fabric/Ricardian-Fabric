6afd15e67f4a4d00dd724942a69c3f7943f55c2a
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-08-19 18:58:14 +0100

    Upload logo refactored

--- lib/view/templates/dropdowns/permawebDropdown.ts
+++ lib/view/templates/dropdowns/permawebDropdown.ts
@@ -12,23 +12,16 @@ export const PermawebDropdown = (
   page: PageState
 ) => {
   return html`
-    <input
-      type="checkbox"
-      id="permaweb_checkbox_toggle"
-      class="dropdown_checkbox_toggle"
-    />
-    <button
-      class="labelButton dropdown_checkbox_label lightCoral-shadow"
-      id="permaweb_checkbox_button"
-      for="permaweb_checkbox_toggle"
-    >
+    <input type="checkbox" id="permaweb_checkbox_toggle" class="dropdown_checkbox_toggle" />
+    <button class="labelButton dropdown_checkbox_label lightCoral-shadow" id="permaweb_checkbox_button"
+      for="permaweb_checkbox_toggle">
       ${arweaveLogo()} Permaweb
     </button>
     <ul>
       <li>
         <button id="Account-popup-button" class="dropdown-button">
-         <span class="fire">🔥</span> 
-        <small> Burner Wallet</small>
+          <span class="fire">🔥</span>
+          <small> Burner Wallet</small>
       </li>
       ${getDropdowns(contractType, page)}
     </ul>
@@ -43,7 +36,7 @@ function getDropdowns(contractType: ContractTypes, page: PageState) {
           return html`
             <li>
               <button id="upload-popup-button" class="dropdown-button">
-                <small>${uploadLogo()} Upload File</small>
+                <small>${uploadLogo("24px", "24px")} Upload File</small>
               </button>
             </li>
           `;
@@ -51,7 +44,7 @@ function getDropdowns(contractType: ContractTypes, page: PageState) {
           return html`
             <li>
               <button id="upload-popup-button" class="dropdown-button">
-                <small>${uploadLogo()} Upload File</small>
+                <small>${uploadLogo("24px", "24px")} Upload File</small>
               </button>
             </li>
             <li>
@@ -61,16 +54,16 @@ function getDropdowns(contractType: ContractTypes, page: PageState) {
             </li>
           `;
         case PageState.trails:
-          return html` <li>
-              <button id="upload-popup-button" class="dropdown-button">
-                <small>${uploadLogo()} Upload File</small>
-              </button>
-            </li>
-            <li>
-              <button class="dropdown-button" id="upload-comment">
-                <small>${CommentLogo()} Add Comment</small>
-              </button>
-            </li>`;
+          return html`<li>
+  <button id="upload-popup-button" class="dropdown-button">
+    <small>${uploadLogo("24px", "24px")} Upload File</small>
+  </button>
+</li>
+<li>
+  <button class="dropdown-button" id="upload-comment">
+    <small>${CommentLogo()} Add Comment</small>
+  </button>
+</li>`;
         default:
           break;
       }
