7c12ef71f21ea7bb66814c91da7f628a7fe7e10b
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-08-24 17:56:33 +0100

    added logos to conenctWalletButton

--- lib/view/templates/pages/connectWalletPage.ts
+++ lib/view/templates/pages/connectWalletPage.ts
@@ -1,20 +1,27 @@
-import { html } from "lit-html";
-import { ConnectWalletLogo, MetamaskLogo } from "../components/logos";
+import { html, nothing } from "lit-html";
+import { BraveLogo, ConnectWalletLogo, MathWalletLogo, MetamaskLogo } from "../components/logos";
 
 export const ConnectWalletPage = () =>
   html`
     <div class="intro-logo">
       <div class="">Ricardian Fabric</div>
-
+    
       <div class="center height200">
-        <button
-          id="connectWalletButton"
-          class="labelButton width-200 marginTop-50 unselectable light-shadow"
-        >
-          <h2>Connect Your Wallet!</h2>
-          ${ConnectWalletLogo()}
+        <button id="connectWalletButton" class="labelButton width-200 marginTop-50 unselectable light-shadow">
+          <h2>Connect Your Wallet</h2>
+          ${SupportedWallets()}
           <hr />
         </button>
       </div>
     </div>
   `;
+
+export const SupportedWallets = () => html`
+<div class="column text-align-center">
+  <div class="rowAround center">
+    ${WindowDotWeb3WalletLogos()}
+  </div>
+</div>
+`;
+
+const WindowDotWeb3WalletLogos = () => html`${MetamaskLogo("40", "40")} ${BraveLogo("40", "40")} ${MathWalletLogo("40", "40")}`
