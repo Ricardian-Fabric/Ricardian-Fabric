cb30882a0cc0019d2d0fc054494cb8b4b52388e1
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-09-11 17:40:01 +0100

    updated to use Bundlr network

--- lib/business/actions/areYouSureButtons.ts
+++ lib/business/actions/areYouSureButtons.ts
@@ -20,26 +20,22 @@ export function areYouSureButtons(props: State) {
     dispatch_noButtonPressed(props);
     dispatch_enableCreateInputs();
   };
-  // Are you sure buttons are only for create page only!
+  // Are you sure buttons are for create page only!
   yesButton.onclick = async function () {
     dispatch_renderLoadingIndicator("transaction-display");
     dispatch_yesButtonPressed(props);
 
-    try {
-      // Upload to Arweave 
-      await postTransaction(props.stashedDetails?.arweaveTx);
-    } catch (err) {
-      dispatch_renderError("Failed to upload the contract!");
+    if (props.stashedDetails?.isBundlr) {
+      await postBundlrNetworkTransaction(props);
+    } else {
+      await postBurnerWalletTransaction(props);
     }
+  };
+}
 
-    try {
-      await postTransaction(props.stashedDetails?.tipTransaction);
-
-    } catch (err) {
-      dispatch_renderError("Failed to send profit sharing transaction!")
-    }
-
-
+async function postBundlrNetworkTransaction(props: State) {
+  try {
+    await props.stashedDetails?.arweaveTx.upload();
 
     const id = props.stashedDetails?.arweaveTx.id;
     const url = geArweaveIdUrl(id);
@@ -48,13 +44,40 @@ export function areYouSureButtons(props: State) {
 
     const smartContract = props.stashedDetails?.smartContract as string;
 
-    if (props.contracttype === ContractTypes.create) {
-      dispatch_deployAgain(props);
-    }
-    if (smartContract !== "NONE") {
-      await smartContractActions(props, url, smartContract);
-    }
-  };
+    dispatch_deployAgain(props);
+    await smartContractActions(props, url, smartContract);
+  } catch (err) {
+    dispatch_renderError("Failed to upload the contract!");
+  }
+}
+
+async function postBurnerWalletTransaction(props) {
+  try {
+    // Upload to Arweave
+    await postTransaction(props.stashedDetails?.arweaveTx);
+  } catch (err) {
+    dispatch_renderError("Failed to upload the contract!");
+  }
+
+  try {
+    await postTransaction(props.stashedDetails?.tipTransaction);
+  } catch (err) {
+    dispatch_renderError("Failed to send profit sharing transaction!");
+  }
+
+  const id = props.stashedDetails?.arweaveTx.id;
+  const url = geArweaveIdUrl(id);
+
+  dispatch_renderTransaction(props, url);
+
+  const smartContract = props.stashedDetails?.smartContract as string;
+
+  if (props.contracttype === ContractTypes.create) {
+    dispatch_deployAgain(props);
+  }
+  if (smartContract !== "NONE") {
+    await smartContractActions(props, url, smartContract);
+  }
 }
 
 export async function smartContractActions(
@@ -74,5 +97,3 @@ export async function smartContractActions(
     dispatch_renderError(options.error);
   }
 }
-
-
