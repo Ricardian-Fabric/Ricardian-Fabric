61a0890ea2c06f018fa586977b97385fed1686d9
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-09-14 23:09:59 +0100

    added <hr/> to bottom of popup!

--- lib/view/templates/popups/deployMissingContractPopup.ts
+++ lib/view/templates/popups/deployMissingContractPopup.ts
@@ -4,12 +4,14 @@ import { html } from "lit-html";
 
 export function missingContractPopup() {
   return html`<h2>Missing Smart Contract</h2>
-    <p>You need to use a Smart Contract with the Ricardian Contract.</p>
-    <p>Add it to the configuration or deploy it now!</p>
-    <div class="row">
-      <button class="labelButton" id="deploySCButton">
-        Deploy Smart Contract
-      </button>
-      <button class="labelButton" id="closePopupButton">Close</button>
-    </div> `;
+<p>You need to use a Smart Contract with the Ricardian Contract.</p>
+<p>Add it to the configuration or deploy it now!</p>
+<div class="row">
+  <button class="labelButton" id="deploySCButton">
+    Deploy Smart Contract
+  </button>
+  <button class="labelButton" id="closePopupButton">Close</button>
+</div>
+<hr />
+    `;
 }
